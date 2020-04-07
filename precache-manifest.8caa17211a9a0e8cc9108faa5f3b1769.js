7836267ffe8f8a523a840443c8a679cf5ba0036c
ameykharote <ameykharote@gmail.com>
2020-04-07 19:58:19 +0530

    Updates

---
+++ precache-manifest.8caa17211a9a0e8cc9108faa5f3b1769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5158d1b7f93af834e1b5f6b8771e9a26",
+    "url": "/image-crop-gh/index.html"
+  },
+  {
+    "revision": "d33ba0b49013e11bc790",
+    "url": "/image-crop-gh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88cd7668a78add523a1f",
+    "url": "/image-crop-gh/static/js/2.19c63307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-crop-gh/static/js/2.19c63307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33ba0b49013e11bc790",
+    "url": "/image-crop-gh/static/js/main.9faea973.chunk.js"
+  },
+  {
+    "revision": "9bbf31ac111bf4fb3107",
+    "url": "/image-crop-gh/static/js/runtime-main.371d9976.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/image-crop-gh/static/media/logo.5d5d9eef.svg"
+  }
+]);
